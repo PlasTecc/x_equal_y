68c612d834be505aa9e74a91213ec1cdad404a2f
PlasTecc <unconfigured@null.spigotmc.org>
2022-11-13 16:27:53 +0300

    Added game loop to main.py

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
-from settings import *
+from settings import DATA
+from functions import *
 
 
 def main():
-    pass
+    while True:
+        print(f"Q - QUIT\t|\tSCORE: {DATA['score']}")
+        numbers = "".join(map(str, generate_numbers())) if DATA['numbers'] is None else DATA['numbers']
+        print(" ".join(numbers))
+        try:
+            while True:
+                user_input = input("> ").lower()
+                if user_input == "q":
+                    raise KeyboardInterrupt
+
+                if validate_input(user_input, numbers):
+                    DATA['score'] += 1
+                    DATA['numbers'] = None
+                    break
+
+        except KeyboardInterrupt:
+            save_changes(numbers)
+            exit(1)
 
 
 if __name__ == '__main__':
