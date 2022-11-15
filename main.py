3df3de05d4c6c4b41863247af0cc0045f040e585
PlasTecc <unconfigured@null.spigotmc.org>
2022-11-15 14:19:39 +0300

    Added clarity to the goal of the game and added skip function.

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-from functions import *
+from functions import generate_numbers, validate_input, save_changes, DATA, ANSWER
 
 
 def main():
     while True:
-        print(f"Q - QUIT\t|\tSCORE: {DATA['score']}")
+        print(f"Q - QUIT\t|\tS - SKIP\t|\tSCORE: {DATA['score']}")
         numbers = "".join(map(str, generate_numbers())) if DATA['numbers'] is None else DATA['numbers']
-        print(" ".join(numbers))
+        print(" ".join(numbers) + f" = {ANSWER}")
         try:
             while True:
                 user_input = input("> ").lower()
                 if user_input == "q":
                     raise KeyboardInterrupt
+
+                if user_input == "s":
+                    DATA['numbers'] = None
+                    break
 
                 if validate_input(user_input, numbers):
                     DATA['score'] += 1
